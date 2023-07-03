78a8ed51d0882987b77a35311cac28a248212252
ApuEmdad <emdadul.apu@savethechildren.org>
2023-07-03 23:03:17 +0600

    register & login successfully done

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ export const register = async (req, res) => {
       lastName,
       email,
       password,
-      picuturePath,
+      picturePath,
       friends,
       location,
       occupation,
@@ -24,17 +24,20 @@ export const register = async (req, res) => {
       lastName,
       email,
       password: passwordHash,
-      picuturePath,
+      picturePath,
       friends,
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 100000),
       impressions: Math.floor(Math.random() * 100000),
     });
-    const savedUser = newUser.save();
+    console.log(newUser);
+    const savedUser = await newUser.save();
+    console.log(savedUser);
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({ message: err.essage });
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
   }
 };
 /* ---- REGISTER USER ---- */
@@ -42,26 +45,29 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email: email });
 
     if (!user) {
-      res.status(400).json({ mag: "Bad request. User does not exist" });
+      return res.status(400).json({ mag: "Bad request. User does not exist" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) {
-      res.status(400).json({ mag: "Wrong Credentials" });
+      return res.status(400).json({ mag: "Wrong Credentials" });
     }
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
-      process.eventNames.JWT_SECRET,
+      process.env.JWT_SECRET,
       { expiresIn: "30d" }
     );
+    console.log("token:", token);
     delete user.password;
     res.status(201).json({ token, user });
   } catch (err) {
-    res.status(500).json({ message: err.essage });
+    res.status(500).json({ message: err.message });
   }
 };
 
